d158508f9ddfb16eb02b44aad1db0d6220d06736
Antonio Bennett <bennettantonio199@gmail.com>
2021-04-11 21:34:48 -0400

    configured floaterm height and width

--- nvim/lua/p-floaterm/init.lua
+++ nvim/lua/p-floaterm/init.lua
@@ -7,8 +7,8 @@ vim.g.floaterm_keymap_new = '<F4>'
 
 vim.g.floaterm_gitcommit = 'floaterm'
 vim.g.floaterm_autoinsert = 1
-vim.g.floaterm_width = 0.8
-vim.g.floaterm_height = 0.8
+vim.g.floaterm_width = 0.9
+vim.g.floaterm_height = 0.9
 -- vim.g.floaterm_wintitle = 0
 vim.g.floaterm_autoclose = 1
 
